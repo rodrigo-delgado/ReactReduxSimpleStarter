a1bb82bc0a7564a6109b4c32387ff7714b7356ee
rodrigo-delgado <rod.delgado85@gmail.com>
2017-03-01 14:47:18 +1300

    refact test code

--- test/components/comment_box.test.js
+++ test/components/comment_box.test.js
@@ -2,16 +2,20 @@ import { renderComponent, expect} from '../test_helper'
 import CommentBox from '../../src/components/comment_box'
 
 describe('CommentBox', () => {
-  it('has a text area', () => {
-    const component = renderComponent(CommentBox)
-    expect(component.find('textarea')).to.exist
-  })
-  it('has a button', () => {
-    const component = renderComponent(CommentBox)
-    expect(component.find('button')).to.exist
-  })
-  it('has a className of comment-box', () => {
-    const component = renderComponent(CommentBox)
-    expect(component).to.have.class('comment-box')
-  })
+  let component;
+
+  beforeEach(() => {
+    component = renderComponent(CommentBox)
+    })
+
+    it('has a text area', () => {
+      expect(component.find('textarea')).to.exist
+    })
+    it('has a button', () => {
+      expect(component.find('button')).to.exist
+    })
+    it('has a className of comment-box', () => {
+      expect(component).to.have.class('comment-box')
+    })
+
 })
