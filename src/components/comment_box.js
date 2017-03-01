385be2cb6614c27fd2e44a7acb9b26407e8fec2c
rodrigo-delgado <rod.delgado85@gmail.com>
2017-03-01 16:13:06 +1300

    handleSubmit passes the test

--- src/components/comment_box.js
+++ src/components/comment_box.js
@@ -10,13 +10,19 @@ export default class CommentBox extends Component {
   handleChange(event) {
     this.setState({ comment: event.target.value})
   }
+  handleSubmit(event) {
+    event.preventDefault()
+
+    this.setState({comment:''})
+  }
 
   render() {
     return (
-      <div className='comment-box'>
-        <textarea value={this.state.comment} onChange={this.handleChange.bind(this)}/>
-        <button>Submit area</button>
-      </div>
+      <form onSubmit={this.handleSubmit.bind(this)} className='comment-box'>
+        <textarea
+        value={this.state.comment} onChange={this.handleChange.bind(this)}/>
+        <button>Submit Comment</button>
+      </form>
     )
   }
 }
